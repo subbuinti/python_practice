22123358ea03389aec29a3f61baa81ba90db59c1
subbuinti <subbuinti1@gmail.com>
2021-08-19 16:33:23 +0530

    For this problem, the prefilled code will contain a function. Write a program that the given function will return the second character in the word passed to the function.

---
+++ functions/secondcharinword.py
@@ -0,0 +1,9 @@
+def second_character(arg_1):
+    # Complete this function
+    character = arg_1[1]
+    return character
+
+word = input()
+# Call the second_character function
+result = second_character(word)
+print(result)
