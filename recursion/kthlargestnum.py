d38cef6e0b0e80c51f6e8c833b8da7bf2e168c46
subbuinti <subbuinti1@gmail.com>
2021-08-20 20:00:09 +0530

    The output should be a single line containing the Kth largest number in the list.

---
+++ recursion/kthlargestnum.py
@@ -0,0 +1,12 @@
+numbers = input()
+k = int(input())
+
+num_list = numbers.split(",")
+
+length_of_num_list = len(num_list)
+for i in range(length_of_num_list):
+    num_list[i] = int(num_list[i])
+
+num_list = sorted(num_list)
+kth_largest_number = num_list[-k]
+print(kth_largest_number)
