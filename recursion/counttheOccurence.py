f71b0b74ee6b071e3d541e86f167b12311569b45
subbuinti <subbuinti1@gmail.com>
2021-08-20 20:09:24 +0530

    For example, if the given N is 5, the output should be 2, as 5 has two occurrences in the list.

---
+++ recursion/counttheOccurence.py
@@ -0,0 +1,6 @@
+nums_list = [5, 10, 20, 35, 5, 50, 20, 100, 200, 10, 150, 100, 100, 20, 20]
+# Write your code here
+number = int(input())
+
+count_of_occurences = nums_list.count(number)
+print(count_of_occurences)
