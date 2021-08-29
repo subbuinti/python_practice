d7732df126ac99a5a63ab6a4deb4ff1eaa4fde42
subbuinti <subbuinti1@gmail.com>
2021-08-29 19:32:14 +0530

    Given an amount, write a program to find a minimum number of currency notes of different denominations that sum to the given amount. Available note denominations are 1000, 500, 100, 50, 20, 5, 1.

---
+++ NestedConditions/Denomination.py
@@ -0,0 +1,35 @@
+amount = int(input())
+note_1000 = 0
+note_500 = 0
+note_100 = 0
+note_50 = 0
+note_20 = 0
+note_5 = 0
+note_1 = 0
+if amount >= 1000:
+    note_1000 = int(amount / 1000)
+    amount = (amount % 1000)
+if amount >= 500:
+    note_1000 = int(amount / 500)
+    amount = (amount % 500)
+if amount >= 100:
+    note_100 = int(amount / 100)
+    amount = (amount % 100)
+if amount >= 50:
+    note_50 = int(amount / 50)
+    amount =(amount % 50)
+if amount >=20:
+    note_20 = int(amount / 20)
+    amount = (amount % 20)
+if amount >=5:
+    note_5 = int(amount / 5)
+    amount = (amount % 5)
+if amount >=1:
+    note_20 = int(amount / 1)
+    amount = (amount % 1)
+note_1 = amount
+print("100:" + str(note_100))
+print("100:" + str(note_100))
+print("50:" + str(note_50))
+print("10:"+ str(note_10))
+print("1:" + str(note_1))
