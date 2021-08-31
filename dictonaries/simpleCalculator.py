70853d8b373c9fec965050dff515edaace45c454
subbuinti <subbuinti1@gmail.com>
2021-08-31 19:50:55 +0530

    The input will be a single line containing two integers and operator(+, -, *, /, and %) similar to 3 + 5.

---
+++ dictonaries/simpleCalculator.py
@@ -0,0 +1,38 @@
+def IsInt(str):
+    try: 
+        int(str)
+        return True
+    except ValueError:
+        return False
+
+txt = input()
+fields = txt.split()
+
+if len(fields) != 3:
+    print("Error: invalid expression. Expected single line containing two "
+          +"integers and operator similar to '3 + 5'")
+    exit(1)
+
+if not IsInt(fields[0]) or not IsInt(fields[2]):
+    print("Error: one of the numbers is incorrect ")
+    exit(1)
+
+if fields[1] not in set(['+','-','*','/','%']):
+    print("Error: unexpected operator. Expected one of +, -, *, /, and %")
+    exit(1)
+
+operand1 = int(fields[0])
+operand2 = int(fields[2])
+
+if fields[1] == '+':
+    result = operand1 + operand2
+elif fields[1] == '-':
+    result = operand1 - operand2
+elif fields[1] == '*':
+    result = operand1 * operand2
+elif fields[1] == '/':
+    result = operand1 / operand2
+elif fields[1] == '%':
+    result = operand1 % operand2
+
+print(result)
